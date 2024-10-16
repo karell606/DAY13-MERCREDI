3925e2b43b7667629321ed25903b0f6e760bbf3a
karell606 <karell.azabe@epitech.digital>
2024-10-16 18:20:42 +0200

    ex_02bis.js

---
+++ ex_02/ex_02bis.js
@@ -0,0 +1,15 @@
+document.querySelector('.add-button').addEventListener('click', (event) => {
+    event.preventDefault();
+    const newTaskName = document.getElementById('results-textarea').value;
+    if (!newTaskName) {
+        alert('Task name must not be empty');
+        return;
+    }
+    fetch('http://localhost:3000/api/v1/todo', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name: newTaskName })
+    })
+    .then(() => location.reload())
+    .catch(error => console.error('Error adding task:', error));
+});
